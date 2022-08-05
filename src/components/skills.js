4a0a6f52e7ed53a4d4c6423df957fa468c48a5f9
Sanidhya <sharmasanidhya0504@gmail.com>
2022-08-05 10:14:06 +0530

    changed skills images

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,9 +1,10 @@
 import React from "react";
 import icons from "../images/icons.png";
+import "../styles/skills.css";
 
 function Skills() {
   return (
-    <div>
+    <div className="icon--wrap">
       <img className="icons" src={icons}></img>
     </div>
   );
